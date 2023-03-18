6a51dc2b444bc54bfc29a4378d6a95510e305a13
Fajar Nur Wahid <fajarnurwahid12@gmail.com>
2023-03-18 21:03:45 +0700

    Change categories to tab

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ $('.navbar-toggle').click(function(e) {
     $('.navbar-menu').toggleClass('active')
     $('html').toggleClass('overflow-hidden')
 })
+
+$('[data-toggle="tab"]').click(function(e) {
+    e.preventDefault()
+    $(this).parent().find('[data-toggle="tab"]').removeClass('active')
+    $('[data-tab]').removeClass('active')
+    $(this).addClass('active')
+    $('[data-tab="'+$(this).attr('data-target')+'"]').addClass('active')
+})
