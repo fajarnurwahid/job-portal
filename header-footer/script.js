2f51016957d30988d0c685489835376adc85466f
Fajar Nur Wahid <fajarnurwahid12@gmail.com>
2023-03-25 16:10:29 +0700

    Add pages

--- header-footer/script.js
+++ header-footer/script.js
@@ -24,14 +24,6 @@ $('.navbar-toggle').click(function(e) {
     }
 })
 
-$('[data-toggle="tab"]').click(function(e) {
-    e.preventDefault()
-    $(this).parent().find('[data-toggle="tab"]').removeClass('active')
-    $('[data-tab]').removeClass('active')
-    $(this).addClass('active')
-    $('[data-tab="'+$(this).attr('data-target')+'"]').addClass('active')
-})
-
 $('[data-arrow="prev"]').click(function(e) {
     e.preventDefault()
     var width = $($(this).attr('data-slider')).children().eq(0).width()
